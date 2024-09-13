eb1f4f9a3941f10800d58b66978065f7f2e306a5
Gastón Camú <camugaston2002@gmail.com>
2024-09-13 20:46:55 -0300

    btn de cerrar sesion terminado

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from './Navbar.module.css';
@@ -6,7 +6,32 @@ import styles from './Navbar.module.css';
 import { useAuth } from '../../context';
 
 const Navbar = ({}) => {
-	const { user } = useAuth(); 
+	const { user, logout } = useAuth();
+	const [ isMenuOpen, setIsMenuOpen ] = useState(false);
+	const menuRef = useRef(null);
+	
+	const handleUserClick = () => {
+		setIsMenuOpen(!isMenuOpen);
+	};
+
+	const handleLogout = () => {
+		logout();
+		setIsMenuOpen(false);
+	}
+
+	useEffect(() => {
+		const handleClickOutside = (e) => {
+			if (menuRef.current && !menuRef.current.contains(e.target)) {
+				setIsMenuOpen(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [menuRef]);
 
 	return (
 		<div className={styles.navbar}>
@@ -14,10 +39,21 @@ const Navbar = ({}) => {
 				<ul className={styles.elementos}>
 					<div className={styles.left}>
 						<Link to="/home" className={styles.Link}>Inicio</Link>
-						<Link to="/empleados" className={styles.Link}>Empleados</Link>
+						<Link to="/empleados" className={styles.Link} >Empleados</Link>
 					</div>
 					<div className={styles.right}>
-						<li>{user.username}</li>
+						<li ref={menuRef} className={`${styles.userMenu} ${isMenuOpen ? styles.menuOpen : ''}`}>
+							<span onClick={handleUserClick} className={`${styles.username} ${isMenuOpen ? styles.menuOpen : ''}`}>
+								{user.username}
+								{isMenuOpen ? '▲' : '▼'}
+							</span>
+							{isMenuOpen && (
+								<div className={styles.dropDownMenu}>
+									<button onClick={handleLogout}>Cerrar sesión</button>
+								</div>
+							)}
+
+						</li>
 					</div>
 				</ul>
 			</nav>
